afb8ddca188cdb9739cc82e2ec1fb9037ac3ec7f
Dragonkeeper <james.belanger@att.net>
2026-03-27 23:21:18 -0500

    More ES stuff

--- server/main/stratum.js
+++ server/main/stratum.js
@@ -5,93 +5,95 @@ import Text from '../../locales/index.js';
 ////////////////////////////////////////////////////////////////////////////////
 
 // Main Stratum Function
-const Stratum = function (logger, client, config, configMain, template) {
+class Stratum {
+  constructor(logger, client, config, configMain, template) {
 
-  const _this = this;
-  this.logger = logger;
-  this.client = client;
-  this.config = config;
-  this.configMain = configMain;
-  this.template = template;
-  this.text = Text[configMain.language];
+    const _this = this;
+    this.logger = logger;
+    this.client = client;
+    this.config = config;
+    this.configMain = configMain;
+    this.template = template;
+    this.text = Text[configMain.language];
 
-  // Stratum Variables
-  process.setMaxListeners(0);
-  this.forkId = process.env.forkId;
+    // Stratum Variables
+    process.setMaxListeners(0);
+    this.forkId = process.env.forkId;
 
-  // Build Stratum from Configuration
-  /* istanbul ignore next */
-  this.handleStratum = function() {
+    // Build Stratum from Configuration
+    /* istanbul ignore next */
+    this.handleStratum = function () {
 
-    // Build Stratum Server
-    _this.stratum = _this.template.builder(_this.config, _this.configMain, () => {});
+      // Build Stratum Server
+      _this.stratum = _this.template.builder(_this.config, _this.configMain, () => { });
 
-    // Handle Stratum Main Events
-    _this.stratum.on('pool.started', () => {});
-    _this.stratum.on('pool.log', (severity, text) => {
-      _this.logger[severity]('Pool', _this.config.name, [text]);
-    });
+      // Handle Stratum Main Events
+      _this.stratum.on('pool.started', () => { });
+      _this.stratum.on('pool.log', (severity, text) => {
+        _this.logger[severity]('Pool', _this.config.name, [text]);
+      });
 
-    // Handle Stratum Network Events
-    _this.stratum.on('pool.network', (networkData) => {
-      _this.network.handleSubmissions(networkData, () => {});
-    });
+      // Handle Stratum Network Events
+      _this.stratum.on('pool.network', (networkData) => {
+        _this.network.handleSubmissions(networkData, () => { });
+      });
 
-    // Handle Stratum Submission Events
-    _this.stratum.on('pool.share', (shareData, shareValid, blockValid) => {
-      _this.shares.handleSubmissions(shareData, shareValid, blockValid, () => {});
-    });
-  };
+      // Handle Stratum Submission Events
+      _this.stratum.on('pool.share', (shareData, shareValid, blockValid) => {
+        _this.shares.handleSubmissions(shareData, shareValid, blockValid, () => { });
+      });
+    };
 
-  // Output Stratum Data on Startup
-  /* istanbul ignore next */
-  this.outputStratum = function() {
+    // Output Stratum Data on Startup
+    /* istanbul ignore next */
+    this.outputStratum = function () {
 
-    // Build Connected Coins
-    const coins = [_this.config.primary.coin.name];
-    if (_this.config.auxiliary && _this.config.auxiliary.enabled) {
-      coins.push(_this.config.auxiliary.coin.name);
-    }
+      // Build Connected Coins
+      const coins = [_this.config.primary.coin.name];
+      if (_this.config.auxiliary && _this.config.auxiliary.enabled) {
+        coins.push(_this.config.auxiliary.coin.name);
+      }
 
-    // Build Pool Starting Message
-    const output = [
-      _this.text.startingMessageText1(`${ _this.config.name }`),
-      _this.text.startingMessageText2(`[${ coins.join(', ') }]`),
-      _this.text.startingMessageText3(_this.config.settings.testnet ? 'Testnet' : 'Mainnet'),
-      _this.text.startingMessageText4(_this.stratum.statistics.ports.join(', ')),
-      _this.text.startingMessageText5(_this.stratum.statistics.feePercentage * 100),
-      _this.text.startingMessageText6(_this.stratum.manager.currentJob.rpcData.height),
-      _this.text.startingMessageText7(_this.stratum.statistics.difficulty),
-      _this.text.startingMessageText8(_this.stratum.statistics.connections),
-      _this.text.startingMessageText9()];
+      // Build Pool Starting Message
+      const output = [
+        _this.text.startingMessageText1(`${_this.config.name}`),
+        _this.text.startingMessageText2(`[${coins.join(', ')}]`),
+        _this.text.startingMessageText3(_this.config.settings.testnet ? 'Testnet' : 'Mainnet'),
+        _this.text.startingMessageText4(_this.stratum.statistics.ports.join(', ')),
+        _this.text.startingMessageText5(_this.stratum.statistics.feePercentage * 100),
+        _this.text.startingMessageText6(_this.stratum.manager.currentJob.rpcData.height),
+        _this.text.startingMessageText7(_this.stratum.statistics.difficulty),
+        _this.text.startingMessageText8(_this.stratum.statistics.connections),
+        _this.text.startingMessageText9()
+      ];
 
-    // Send Starting Message to Logger
-    if (_this.forkId === '0') {
-      _this.logger['log']('Pool', _this.config.name, output, true);
-    }
-  };
+      // Send Starting Message to Logger
+      if (_this.forkId === '0') {
+        _this.logger['log']('Pool', _this.config.name, output, true);
+      }
+    };
 
-  // Setup Pool Stratum Capabilities
-   
-  /* istanbul ignore next */
-  this.setupStratum = async function() {
-    _this.network = new Network(logger, _this.client, _this.config, _this.configMain);
-    _this.shares = new Shares(logger, _this.client, _this.config, _this.configMain);
-    _this.handleStratum();
-    await new Promise(resolve => _this.stratum.setupPrimaryDaemons(resolve));
-    await new Promise(resolve => _this.stratum.setupAuxiliaryDaemons(resolve));
-    _this.stratum.setupPorts();
-    await new Promise(resolve => _this.stratum.setupSettings(resolve));
-    _this.stratum.setupRecipients();
-    _this.stratum.setupManager();
-    await new Promise(resolve => _this.stratum.setupPrimaryBlockchain(resolve));
-    await new Promise(resolve => _this.stratum.setupAuxiliaryBlockchain(resolve));
-    await new Promise(resolve => _this.stratum.setupFirstJob(resolve));
-    await new Promise(resolve => _this.stratum.setupBlockPolling(resolve));
-    await new Promise(resolve => _this.stratum.setupNetwork(resolve));
+    // Setup Pool Stratum Capabilities
+    /* istanbul ignore next */
+    this.setupStratum = async function () {
+      _this.network = new Network(logger, _this.client, _this.config, _this.configMain);
+      _this.shares = new Shares(logger, _this.client, _this.config, _this.configMain);
+      _this.handleStratum();
+      await new Promise(resolve => _this.stratum.setupPrimaryDaemons(resolve));
+      await new Promise(resolve => _this.stratum.setupAuxiliaryDaemons(resolve));
+      _this.stratum.setupPorts();
+      await new Promise(resolve => _this.stratum.setupSettings(resolve));
+      _this.stratum.setupRecipients();
+      _this.stratum.setupManager();
+      await new Promise(resolve => _this.stratum.setupPrimaryBlockchain(resolve));
+      await new Promise(resolve => _this.stratum.setupAuxiliaryBlockchain(resolve));
+      await new Promise(resolve => _this.stratum.setupFirstJob(resolve));
+      await new Promise(resolve => _this.stratum.setupBlockPolling(resolve));
+      await new Promise(resolve => _this.stratum.setupNetwork(resolve));
 
-    _this.outputStratum();
+      _this.outputStratum();
+    };
   }
-};
+}
 
 export default Stratum;
